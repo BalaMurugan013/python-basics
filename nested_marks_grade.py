843818e100a1cf27353e8d55170e5ce139f5bc4f
BalaMurugan013 <balamurugank013@gmail.com>
2026-03-20 19:28:03 +0530

    Day 4:
    Added nested logic programs and improved UI with card & shadow

---
+++ nested_marks_grade.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 50:
+    if marks >= 90:
+        print("A")
+    else:
+        print("Pass")
+else:
+    print("Fail")
---
+++ nested_positive_even_odd.py
@@ -0,0 +1,9 @@
+n = int(input("Enter number: "))
+
+if n > 0:
+    if n % 2 == 0:
+        print("Positive Even")
+    else:
+        print("Positive Odd")
+else:
+    print("Negative or Zero")
